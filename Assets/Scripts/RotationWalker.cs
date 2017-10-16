91dd4d1e09fc3e17bc891853f048e7fe5756d5f1
Philip Kurowski <p_kurowski@live.ca>
2017-10-16 07:43:53 -0400

    Initial commit
    Created person that walks around a circle

---
+++ Assets/Scripts/RotationWalker.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationWalker : MonoBehaviour {
+    /// <summary>
+    /// The circle that we're rotating around
+    /// </summary>
+    public RectTransform circularBody;
+
+    /// <summary>
+    /// The sprite renderer to rotate while moving
+    /// </summary>
+    public SpriteRenderer sprite;
+
+    /// <summary>
+    /// Move speed
+    /// </summary>
+    public float speed = 1.0f;
+
+    /// <summary>
+    /// The circle's radius
+    /// </summary>
+    private float radius = 0f;
+
+    /// <summary>
+    /// Current degree position on the circle
+    /// </summary>
+    private float theta = 0f;
+
+	// Use this for initialization
+	void Start () {
+        radius = circularBody.sizeDelta.x * circularBody.localScale.x / 2f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        theta += speed * Time.deltaTime;
+
+        transform.position = new Vector2(circularBody.position.x + radius * Mathf.Sin(theta), circularBody.position.y + radius * Mathf.Cos(theta));
+        sprite.transform.eulerAngles = new Vector3(0, 0, -theta * (180 / Mathf.PI));
+	}
+}
