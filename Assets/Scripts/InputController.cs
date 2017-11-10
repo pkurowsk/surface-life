35cda0b17d9bd435f896a678effef9a877f480d3
Philip Kurowski <p_kurowski@live.ca>
2017-11-10 00:52:20 -0500

    Added controllable celestial body

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour {
+
+    public float speed = 100.0f;
+
+    public LinkedSegment trailingSegment;
+
+    private Rigidbody2D rgd2d;
+
+    private Vector2 direction = Vector2.zero;
+
+    private static InputController _instance;
+
+    public static InputController getInstance()
+    {
+        if (!_instance)    {
+            _instance = GameObject.FindObjectOfType<InputController>();
+        }
+
+        return _instance;
+    }
+
+	// Use this for initialization
+	void Start () {
+        rgd2d = GetComponent<Rigidbody2D>();
+        direction = transform.up;
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        if (!Vector2.zero.Equals(input))
+        {
+            direction = input.normalized;
+        }
+
+        rgd2d.velocity = direction * speed * Time.deltaTime;
+        trailingSegment.Follow(transform);
+		
+	}
+}
---
+++ Assets/Scripts/LinkedSegment.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinkedSegment : MonoBehaviour {
+    public LinkedSegment trailingSegment;
+
+    private const float MAX_DIST = 0.5f;
+    private const float SPEED_MOD = 50.0f;
+
+    public void Follow(Transform followingSegment)
+    {
+        if (Vector2.Distance(transform.position, followingSegment.position) >= MAX_DIST)   {
+            transform.position = Vector2.MoveTowards(transform.position, followingSegment.position, InputController.getInstance().speed / SPEED_MOD * Time.deltaTime);
+
+            if (trailingSegment)
+            {
+                trailingSegment.Follow(transform);
+            }
+        }
+	}
+}
